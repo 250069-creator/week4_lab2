b8dd0038006315efbb350ae4ea19ec639396c13d
250069-creator <250069@newuu.uz>
2025-09-17 11:20:27 +0300

    this is main

---
+++ main.cpp
@@ -0,0 +1,134 @@
+// #include <iostream>
+// #include <math.h>
+// using namespace std;
+// int main() {
+// problem 10
+    // char command;
+    // cin >> command;
+    // switch (command) {
+    //     case 'g':
+    //         cout << "Gol" ; break;
+    //     case 'y':
+    //         cout << "Get ready!"; break;
+    //     case 'r':
+    //         cout << "Stop"; break;
+    //     default : cout<< "invalid input"; break;
+    //
+    // }
+
+// problem 11
+    // int grade;
+    // cin >> grade;
+    // switch ( grade / 10) {
+    //     case 10: cout << "A"; break;
+    //     case 9: cout << " A"; break;
+    //     case 8: cout << " B"; break;
+    //     case 7: cout << " C"; break;
+    //     case 6: cout << " D"; break;
+    //     default : cout << " F"; break;
+    // }
+
+    // Problem 12
+    // int a, b, c;
+    // cout << " Enter traingle angles ";
+    // cin >> a >> b >> c;
+    // if ( a>0 && b>0 && c>0 && ( a + b + c) && ( ((a +b)> (c) && ( (a+c)>b) && ((b+c)>a)) )) {
+    //     cout << " The traingle is valid" << endl;
+    // } else {
+    //     cout << " The traingle is not valid" << endl;
+    // }
+
+    // problem 13
+    // double a, b, c;
+    // cout << " Enter the values :";
+    // cin >> a >> b >> c;
+    // double D = b*b - 4*a*c;
+    // double x1, x2;
+    // if ( D>0) {
+    //     x1= ( -b - sqrt(D))/(2*a);
+    //     x2= ( -b + sqrt(D)) /(2*a);
+    //     cout << "x1 = " << x1 << endl;
+    //     cout << " x2 = " << x2 << endl;
+    // }
+    // else if ( D==0) {
+    //     x1=x2 = -b/(2*a);
+    //     cout << " x1 = " << x1 << endl;
+    //     cout << " x2 = " << x2 << endl;
+    // }
+    // else {
+    //     cout << " There is no root for ( D< 0) " << endl;
+    // }
+
+    // Problem 14
+//     char ch;
+//     cout << " Enter character : " ;
+//     cin >> ch;
+//     if ( ch >= 'a' && ch <= 'z') {
+//         cout << " Lowcase alphabet" << endl;
+//     }
+//     else if ( ch >= 'A' && ch <= 'Z') {
+//     cout << " Uppercase alphebet " << endl;
+// }
+//     else {
+//         cout << " It is not an Alphebet " << endl;
+//     }
+
+    // Problem 15
+    // double w1, p1, w2, p2;
+    // cout << " Enter weight and price for package1 : ";
+    // cin >> w1 >> p1;
+    //
+    // cout << " Enter weight and price for package2 :" ;
+    // cin >> w2 >> p2;
+    //
+    // double cost1 = p1/w1;
+    // double cost2 = p2/w2;
+    //
+    // cout << " Cost per unit  for package1 : " << cost1 << endl;
+    // cout << " Cost per unit for package2 : " << cost2 << endl;
+    //
+    // if ( cost1 < cost2 ) {
+    //     cout << " Package 1 has better price " << endl;
+    // } else if ( cost2 < cost1 ) {
+    //     cout << " Package 2 has better price " << endl;
+    // }
+    // else {
+    //     cout << " Two packages have the same price " << endl;
+    // }
+
+    // Problem 16
+
+    // int number;
+    // cout << " Enter three- digit integer : " ;
+    // cin >> number;
+    //
+    // int first = number /100;
+    // int last = number %10;
+    //
+    // if ( first == last ) {
+    //     cout << number << " is palindrome " << endl;
+    // }
+    // else {
+    //     cout << number << " is not palindrome " << endl;
+    // }
+
+    // Problem 17
+
+    // char choice;
+    // cout << " choose a lanhuage ( u - for Uzbek, e - for English, r - for Russian, g- for german) ";
+    // cin >> choice;
+    //
+    // switch (choice ) {
+    //     case 'u':
+    //         cout << "Salom " << endl; break;
+    //     case 'e':
+    //         cout << " Hello " << endl; break;
+    //     case 'r':
+    //         cout << " Privet " << endl; break;
+    //     case 'g':
+    //         cout << " Hallo " << endl; break;
+    //     default:
+    //         cout << " I don't know the language " << endl; break;
+    // }
+    // return 0;
+// }
